1c5672fd7e0f51ea43888f7b30357077986a07c4
jcaylao <jcaylao@ucsd.edu>
2023-01-26 12:46:48 -0800

    lab3

--- ArrayTests.java
+++ ArrayTests.java
@@ -1,4 +1,7 @@
 import static org.junit.Assert.*;
+
+import java.lang.reflect.Array;
+
 import org.junit.*;
 
 public class ArrayTests {
@@ -9,10 +12,22 @@ public class ArrayTests {
     assertArrayEquals(new int[]{ 3 }, input1);
 	}
 
+  @Test
+  public void testReverseInPlaceFail() {
+    int[] input2 = {8,9,10};
+    ArrayExamples.reverseInPlace(input2);
+    assertArrayEquals(new int[]{10,9,8}, input2);
+  }
 
   @Test
   public void testReversed() {
     int[] input1 = { };
     assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+  }
+
+  @Test
+  public void testReversedFail() {
+    int[] input2 = {8, 9, 10};
+    assertArrayEquals(new int[]{10,9,8}, ArrayExamples.reversed(input2));
   }
 }
